cc21851304d2d461dc60a35d3d05fa218a4b26c3
Lisa Welling <lisa.welling@hva.nl>
2026-03-26 15:08:46 +0100

    ...

--- vite.config.js
+++ vite.config.js
@@ -12,5 +12,19 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
+    build: {
+      rollupOptions: {
+        output: {
+          sanitizeFileName: (name) => {
+            // Sanitizes file names generated during the build process:
+            // - Replaces spaces with dashes ('-').
+            // - Removes invalid characters that are not alphanumeric, underscores (_), periods (.), or dashes (-).
+            return name
+              .replace(/\s+/g, '-') // Replaces spaces with dashes.
+              .replace(/[^a-zA-Z0-9_.-]/g, '') // Removes all invalid characters.
+          },
+        },
+      },
+    },
   },
 })
