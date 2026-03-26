ca24fd16fe0c8264a8061d12d776b07e9e858726
Lisa Welling <lisa.welling@hva.nl>
2026-03-26 15:15:05 +0100

    fix?

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), vueDevTools()],
   base: '/BeyondCanvas/',
@@ -12,17 +10,14 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
-    build: {
-      rollupOptions: {
-        output: {
-          sanitizeFileName: (name) => {
-            // Sanitizes file names generated during the build process:
-            // - Replaces spaces with dashes ('-').
-            // - Removes invalid characters that are not alphanumeric, underscores (_), periods (.), or dashes (-).
-            return name
-              .replace(/\s+/g, '-') // Replaces spaces with dashes.
-              .replace(/[^a-zA-Z0-9_.-]/g, '') // Removes all invalid characters.
-          },
+  },
+  build: { 
+    rollupOptions: {
+      output: {
+        sanitizeFileName: (name) => {
+          return name
+            .replace(/\s+/g, '-')
+            .replace(/[^a-zA-Z0-9_.-]/g, '')
         },
       },
     },
