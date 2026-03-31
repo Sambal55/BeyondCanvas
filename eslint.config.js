2c302b8352c34c429f8a84e2d66b5d763e025951
Lisa Welling <lisa.welling@hva.nl>
2026-03-31 15:53:49 +0200

    fix: ESlint manual config

--- eslint.config.js
+++ eslint.config.js
@@ -1,18 +1,14 @@
-import { defineConfig, globalIgnores } from 'eslint/config'
-import globals from 'globals'
 import js from '@eslint/js'
-import pluginVue from 'eslint-plugin-vue'
-import pluginOxlint from 'eslint-plugin-oxlint'
-import skipFormatting from 'eslint-config-prettier/flat'
-
-export default defineConfig([
+import tsParser from '@typescript-eslint/parser'
+import vue from 'eslint-plugin-vue'
+import prettier from 'eslint-config-prettier/flat'
+import oxlint from 'eslint-plugin-oxlint'
+import tsPlugin from '@typescript-eslint/eslint-plugin'
+import globals from "globals"
+export default [
   {
-    name: 'app/files-to-lint',
-    files: ['**/*.{vue,js,mjs,jsx}'],
+    ignores: ['dist/**'],
   },
-
-  globalIgnores(['**/dist/**', '**/dist-ssr/**', '**/coverage/**']),
-
   {
     languageOptions: {
       globals: {
@@ -20,11 +16,48 @@ export default defineConfig([
       },
     },
   },
+  js.configs.recommended, // FIXED
+  ...vue.configs['flat/essential'], // FIXED
+  ...oxlint.configs['flat/recommended'], // FIXED
+  prettier, // FIXED
 
-  js.configs.recommended,
-  ...pluginVue.configs['flat/essential'],
+  {
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+    },
+    rules: {
+      '@typescript-eslint/no-unused-vars': 'error',
+      '@typescript-eslint/no-empty-interface': 'error',
+    },
+  },
 
-  ...pluginOxlint.buildFromOxlintConfigFile('.oxlintrc.json'),
+  {
+    files: ['**/*.{js,mjs,cjs,ts,tsx,vue}'],
+    languageOptions: {
+      parser: vue.parser,
+      parserOptions: {
+        parser: tsParser,
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      vue,
+      oxlint,
+    },
+  },
 
-  skipFormatting,
-])
+  {
+    rules: {
+      camelcase: ['error', { ignoreDestructuring: true }],
+    },
+  },
+]
