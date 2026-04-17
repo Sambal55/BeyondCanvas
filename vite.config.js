04f8bfcd029590446e636de890e628feb811bb95
Lisa Welling <lisa.welling@hva.nl>
2026-04-17 12:04:42 +0200

    feat: QR code when hosting on network

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,10 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import { qrcode } from 'vite-plugin-qrcode'
 
 export default defineConfig(({ mode }) => ({
-  plugins: [vue(), vueDevTools()],
+  plugins: [vue(), vueDevTools(), qrcode()],
 
   // Use correct base for dev vs production
   base: mode === 'production' ? '/BeyondCanvas/' : '/',
