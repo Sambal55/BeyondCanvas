5ce0594d50fcf061eab73a1792962b6b685f5d47
Lisa Welling <lisa.welling@hva.nl>
2026-04-24 13:34:13 +0200

    fix: restore imporantcubes

--- src/components/ImportantPopup.vue
+++ src/components/ImportantPopup.vue
@@ -48,10 +48,10 @@ onUnmounted(() => {
   left: 50%;
   transform: translate(-50%, -50%);
 
-  background: white;
+  background: yellow;
   padding: 2px 20px;
   border-radius: 12px;
-  box-shadow: 0 4px 20px rgba(0, 0, 0, 0.25);
+  box-shadow: 0 4px 20px black;
 
   max-width: 80%;
   z-index: 95;
