303933c8eed6bf301a2c2e0163c417d375941449
Sambal55 <Sambal55@users.noreply.github.com>
2026-04-29 17:13:01 +0000

    deploy: a2af546d295337731900f93f0e3462e62c5950bb

---
+++ assets/useTTSStore-1amF4WD3.js
@@ -0,0 +1,1 @@
+import{f as s,g as n}from"./index-DaQJZ9Qp.js";const r=s("tts",()=>{const e=n(!0);function t(){e.value=!e.value,e.value||speechSynthesis.cancel()}return{enabled:e,toggle:t}});export{r as u};
--- assets/useTTSStore-I95-4vUj.js
+++
@@ -1,1 +0,0 @@
-import{f as s,g as n}from"./index-eZ7QJLgQ.js";const r=s("tts",()=>{const e=n(!0);function t(){e.value=!e.value,e.value||speechSynthesis.cancel()}return{enabled:e,toggle:t}});export{r as u};
