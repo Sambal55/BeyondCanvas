dcb2f73e15dbef973488cf16e7fb57650cc28a46
Sambal55 <Sambal55@users.noreply.github.com>
2026-05-04 11:46:45 +0000

    deploy: e77a888f386f48afd8ceeaef79694604c6bcf5e1

--- assets/useTTSStore-COX0rlnW.js
+++
@@ -1,1 +0,0 @@
-import{d as s,e as n}from"./index-BE0A8x2N.js";const r=s("tts",()=>{const e=n(!0);function t(){e.value=!e.value,e.value||speechSynthesis.cancel()}return{enabled:e,toggle:t}});export{r as u};
---
+++ assets/useTTSStore-JITx9qyd.js
@@ -0,0 +1,1 @@
+import{d as s,e as n}from"./index-DrnklT1T.js";const r=s("tts",()=>{const e=n(!0);function t(){e.value=!e.value,e.value||speechSynthesis.cancel()}return{enabled:e,toggle:t}});export{r as u};
