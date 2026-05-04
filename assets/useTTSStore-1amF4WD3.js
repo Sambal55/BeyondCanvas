129326bb23b8a8cdb59c5db02a7c85996567bb7c
Sambal55 <Sambal55@users.noreply.github.com>
2026-05-04 11:14:49 +0000

    deploy: 8bd471e168e2f8cbf036217b10136283e109ff5b

--- assets/useTTSStore-1amF4WD3.js
+++
@@ -1,1 +0,0 @@
-import{f as s,g as n}from"./index-DaQJZ9Qp.js";const r=s("tts",()=>{const e=n(!0);function t(){e.value=!e.value,e.value||speechSynthesis.cancel()}return{enabled:e,toggle:t}});export{r as u};
