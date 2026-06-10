457005ffb188aedc08c4338a7d5972d6cb7762d2
Lisa Welling <lisa.welling@hva.nl>
2026-06-10 15:10:04 +0200

    fix: always show manual

--- src/stores/useGuideStore.ts
+++ src/stores/useGuideStore.ts
@@ -2,17 +2,15 @@ import { defineStore } from 'pinia'
 
 export const useGuideStore = defineStore('guide', {
   state: () => ({
-    isVisible: false,
+    isVisible: true, // Always visible
   }),
 
   actions: {
     openGuide() {
-      if (localStorage.getItem('guideSeenBefore')) return
       this.isVisible = true
     },
     closeGuide() {
       this.isVisible = false
-      localStorage.setItem('guideSeenBefore', 'true')
     },
   },
 })
